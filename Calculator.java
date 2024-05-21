cb87df632688e588cd3eb90a3d356e2a96768d67
Dimo Mitev <dimosoftuni@gmail.com>
2024-05-21 21:35:32 +0300

    Added comment locally

--- Calculator.java
+++ Calculator.java
@@ -17,6 +17,7 @@ public class Calculator {
     }
 
     // Method for division
+	// There is a check for deletion by zero
     public double divide(double a, double b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed.");
